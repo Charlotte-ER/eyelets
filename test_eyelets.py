f0b003198bca16a9b5776ee1b68896e742e5e469
Charlotte-ER <68346113+Charlotte-ER@users.noreply.github.com>
2023-02-04 09:25:28 +0000

    Set up tests to be written.

---
+++ test_eyelets.py
@@ -0,0 +1,26 @@
+import pytest
+from eyelets import get_user_input, calculate_standard_distance_between_eyelets, title, create_instructions, create_diagram
+
+def test_get_user_input():
+    # get_user_input():
+    ...
+
+
+def test_calculate_standard_distance_between_eyelets(opening, position, number):
+    # calculate_standard_distance_between_eyelets(opening, position, number):
+    ...
+
+
+def test_title(title):
+    # title(title):
+    ...
+
+
+def test_create_instructions(opening, position, standard, number):
+    # create_instructions(opening, position, standard, number):
+    ...
+
+
+def test_create_diagram(standard, number):
+    # create_diagram(standard, number):
+    ...
