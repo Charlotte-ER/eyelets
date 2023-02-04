db396291ef9000c364e9d95df7da763b294c387b
Charlotte-ER <68346113+Charlotte-ER@users.noreply.github.com>
2023-02-04 09:01:25 +0000

    Refactor so functions return values rather than print instructions and diagram.

--- eyelets.py
+++ eyelets.py
@@ -4,71 +4,58 @@
 
 def main():
     opening, position, number = get_user_input()
+    standard = calculate_standard_distance_between_eyelets(opening, position, number)
 
-    standard = calculate_standard_distance(opening, position, number)
-    
-    print_instructions(position, number, standard)
-    print_diagram(standard, number)
+    print(create_instructions(opening, position, standard, number))
+    print(create_diagram(standard, number))
 
 
 def get_user_input():
-    # Get length of opening
-    opening = round(float(input("How long is your opening? ")),1)
-
-    # Get desired start and end position
-    position = round(float(input("How far down from the top edge do you want your first eyelet to be? ")),1)
-
-    # Get desired number of laces
-    number = int(input("How many cross-laces do you want (excluding top and bottom laces)? "))
-
+    opening = round(float(input('How long is your opening? ')),1)
+    position = round(float(input('How far down from the top edge do you want your first eyelet to be? ')),1)
+    number = int(input('How many cross-laces do you want (excluding top and bottom laces)? '))
     return opening, position, number
 
 
-def calculate_standard_distance(opening, position, number):
-    # Calculate standard distance between each eyelet
+def calculate_standard_distance_between_eyelets(opening, position, number):
     return round(((opening - (position * 2)) / (number + 0.5)),1)
 
 
-def print_instructions(position, number, standard):
-    # Output Instructions
-    print("\n---------------------------------------------------------------------------------------------------------")
-    print("INSTRUCTIONS")
-    print("---------------------------------------------------------------------------------------------------------")
-
-    print(f"\nStarting from the top edge of your opening, mark eyelets {position} down from the top, on both sides.")
-    print(f"Starting from the bottom edge of your opening, mark eyelets {position} up from the bottom, on both sides.")
-    print(f"\nOn the LEFT hand side of the opening:")
-    print(f"\tmark an eyelet {standard/2} down from the first eyelet")
-    print(f"\tmark the other {number-1} eyelets, each {standard} down from the eyelet above.")
-    print(f"\nOn the RIGHT hand side of the opening:")
-    print(f"\tmark an eyelet {standard} down from the first eyelet")
-    print(f"\tmark the other {number-1} eyelets, each {standard} down from the eyelet above.")
-    print(f"\tthe gap between the bottom two eyelets on the right should measure {standard/2}.")
+def title(title):
+    return f'\n{"-" * 100} \n{title.upper()}\n{"-" * 100}\n\n'
 
 
-def print_diagram(standard, number):
-    # Output Diagram
-    print("\n---------------------------------------------------------------------------------------------------------")
-    print("DIAGRAM")
-    print("---------------------------------------------------------------------------------------------------------")
-    # First row
-    print('\n')
-    print(f"\tX\tX")
-    print(f"\t{(standard)/2}\t|")
+def create_instructions(opening, position, standard, number):
+    return (f'{title("Instructions")}'
 
-    # Laces
+    f'Length of Opening: {opening}\n'
+    f'Position of first eyelet: {position} down from top edge\n'
+    f'Number of diagonal cross-laces: {number}\n\n'
+
+    f'Starting from the top edge of your opening, mark eyelets {position} down from the top, on both sides.\n'
+    f'Starting from the bottom edge of your opening, mark eyelets {position} up from the bottom, on both sides.\n\n'
+    f'On the LEFT hand side of the opening:\n'
+    f'\tmark an eyelet {standard/2} down from the first eyelet\n'
+    f'\tmark the other {number-1} eyelets, each {standard} down from the eyelet above.\n'
+    f'\nOn the RIGHT hand side of the opening:\n'
+    f'\tmark an eyelet {standard} down from the first eyelet\n'
+    f'\tmark the other {number-1} eyelets, each {standard} down from the eyelet above.\n'
+    f'\tthe gap between the bottom two eyelets on the right should measure {standard/2}.\n')
+
+
+def create_diagram(standard, number):
+    heading = f'{title("Diagram")}'
+    first_row = f'\tX\tX\n\t{(standard)/2}\t|\n'
+    laces = ''
+    last_row = f'\t|\t{(standard)/2}\n\tX\tX\n'
+    footer = f'\n{"-"*100}'
+
     for i in range(number):
-        print(f"\tX\t{(standard)}")
-        print(f"\t|\t|")
-        print(f"\t{(standard)}\tX")
+        laces = laces + (f'\tX\t{(standard)}\n\t|\t|\n\t{(standard)}\tX\n')
         if i < (number-1):
-            print(f"\t|\t|")
+            laces = laces + (f'\t|\t|\n')
 
-    # Last row
-    print(f"\t|\t{(standard)/2}")
-    print(f"\tX\tX")
-
-    print("---------------------------------------------------------------------------------------------------------")
+    return heading + first_row + laces + last_row + footer
 
 
 if __name__ == "__main__":
